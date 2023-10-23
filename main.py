64282d9187249457523a4646cf487eaee2334c2c
Embreaon <qhdtnals9803@gmail.com>
2023-10-23 17:54:15 +0900

    Fix favicon 404 error

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 from pydantic import BaseModel
 
@@ -15,6 +17,12 @@ app.mount("/static", StaticFiles(directory="statics"), name="static")
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("main.html", {"request":request})
+
+@app.get("/favicon.ico")
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = os.path.join(app.root_path, "static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
 
 @app.get("/menu")
 async def menu(request: Request):
