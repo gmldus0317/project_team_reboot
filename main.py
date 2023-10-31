1b8d6a50b95bd8a1852699bede2abf98cb1e7c0a
Embreaon <qhdtnals9803@gmail.com>
2023-10-31 16:44:04 +0900

    Cart Logic

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import cryptography # 이 라이브러리가 없으면 오류 발생
 
-from fastapi import FastAPI, Depends, Form, status
+from fastapi import FastAPI, Depends
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
 
 from admin import *
 
@@ -20,6 +19,8 @@ app.mount("/static", StaticFiles(directory="statics"), name="static")
 app.include_router(admin)
 engine = engineconn()
 
+cart: List[Cart_list] = []
+
 def get_db():
     session = engine.session_maker()
     try:
@@ -27,22 +28,30 @@ def get_db():
     finally:
         session.close()
 
-order_list = []
-
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("main.html", {"request":request})
 
-@app.get("/menu")
+@app.get("/menu", response_model=List[Cart_list])
 async def menu(request: Request, db: Session = Depends(get_db)):
     dt_coffee = db.query(Menu).filter(Menu.category == "커피").order_by(Menu.menu_id.desc())
     dt_tea = db.query(Menu).filter(Menu.category == "티").order_by(Menu.menu_id.desc())
     dt_drink = db.query(Menu).filter(Menu.category == "음료").order_by(Menu.menu_id.desc())
     dt_dessert = db.query(Menu).filter(Menu.category == "디저트").order_by(Menu.menu_id.desc())
-    return templates.TemplateResponse("menu.html", {"request":request, "dt_coffee": dt_coffee, "dt_tea": dt_tea, "dt_drink": dt_drink, "dt_dessert": dt_dessert})
+    return templates.TemplateResponse("menu.html", {"request":request, "dt_coffee": dt_coffee, "dt_tea": dt_tea, "dt_drink": dt_drink, "dt_dessert": dt_dessert, "cart_list": cart})
 
 @app.get("/info/{menu_id}")
-async def info(request: Request, menu_id: int, db: Session = Depends(get_db)):
+async def notice(request: Request, menu_id: int, db: Session = Depends(get_db)):
     crn_info = db.query(Menu).filter(Menu.menu_id == menu_id).first() # current_information
     dt_info = db.query(Menu).order_by(Menu.menu_id.desc())
     return templates.TemplateResponse("notice.html", {"request": request, "dt_info": dt_info, "crn_info": crn_info})
+
+@app.get("/cart/add/{menu_nm}")
+async def add_cart(request: Request, menu_nm: str):
+    cart.append(menu_nm)
+    return RedirectResponse(url=app.url_path_for("menu"), status_code=status.HTTP_303_SEE_OTHER)
+
+@app.delete("/cart/delete/")
+async def remove_cart():
+    cart.clear()
+    return {"msg": "Cart Cleared"}
